5a3f6f83e70c5df769bcdd6071ab328e004b5db1
Steph Crown <emmanuelstephen024@gmail.com>
2024-06-27 19:14:58 +0100

    Delete dist folder

--- dist/index.2c2969d1.js
+++
@@ -1,8 +0,0 @@
-const $ = (id)=>{
-    return document.getElementById(id);
-};
-const _ = (selector)=>{
-    return document.querySelector(selector);
-};
-
-//# sourceMappingURL=index.2c2969d1.js.map
