cc14a4b90ade3c3d502292bcd61b2aa45ee9ac9a
Samuel Aribigbola <ayoolasamuel2302@gmail.com>
2024-11-18 19:36:22 +0100

    Samuel's Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,21 @@ module.exports = {
         "uranium-blue": "#B3D5FC",
         "indian-red": "#DB5764",
       },
+      keyframes: {
+        fanOut: {
+          '0%': {
+            transform: 'translate(-50%, -50%) scale(0.8) rotate(0deg)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translate(0, 0) scale(1) rotate(var(--tw-rotate))',
+            opacity: '1',
+          },
+        },
+      },
+      animation: {
+        fanOut: 'fanOut 0.8s ease-out forwards',
+      },
     },
   },
   plugins: [],
